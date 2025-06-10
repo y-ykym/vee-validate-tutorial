dba301f0e66fbb9bd98bd24f2e7dba63f8b4bce9
yokoyama <yokoyama@social-db.co.jp>
2025-06-10 14:56:36 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
   <div id="app">
-    <form novalidate @submit.prevent="onSubmit">
-      <input type="email" name="email" />
-      <button>Sign up for newsletter</button>
-    </form>
+    <Form @submit="onSubmit">
+      <Field name="email" type="email" :rules="validateEmail" />
+      <ErrorMessage name="email" />
+      <button>Sign up</button>
+    </Form>
   </div>
 </template>
 <script>
+import { Form, Field, ErrorMessage } from 'vee-validate';
 export default {
+  components: {
+    Form,
+    Field,
+    ErrorMessage,
+  },
   methods: {
-    onSubmit() {
-      console.log('Submitted');
+    onSubmit(values) {
+      console.log(values, null, 2);
+    },
+    validateEmail(value) {
+      // if the field is empty
+      if (!value) {
+        return 'This field is required';
+      }
+      // if the field is not a valid email
+      const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+      if (!regex.test(value)) {
+        return 'This field must be a valid email';
+      }
+      // All is good
+      return true;
     },
   },
 };
